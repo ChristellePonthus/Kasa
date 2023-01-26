a73a9f5a4cfcb78ea46d0a66f6943668d8a4ebff
christelledp <christelledp@sfr.fr>
2023-01-26 13:24:59 +0100

    Carrousel mis en place et fonctionnel

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -4,13 +4,26 @@ import { useState } from "react";
 const Carrousel = ({ logement }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const firstSlide = currentIndex === 0;
+  const lastSlide = currentIndex === logement.pictures.length - 1;
+  let newIndex;
+  function goToPrev() {
+    newIndex = firstSlide ? logement.pictures.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+
+  function goToNext() {
+    newIndex = lastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
+
   return (
     <section className="carrousel" key={logement.id}>
       <div className="img">
         <img src={logement.pictures[currentIndex]} alt={logement.title} />
       </div>
-      <button className="leftArrow" />
-      <button className="rightArrow" />
+      <button onClick={goToPrev} className="leftArrow" />
+      <button onClick={goToNext} className="rightArrow" />
     </section>
   );
 };
