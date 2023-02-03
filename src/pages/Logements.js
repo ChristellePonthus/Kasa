b30005fefad8256a74e06a732bec05184364bcbd
christelledp <christelledp@sfr.fr>
2023-02-03 12:55:48 +0100

    Redirect 404 si id incorrect

--- src/pages/Logements.js
+++ src/pages/Logements.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { render } from "react-dom";
-import { redirect, useParams, useRouteError } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LogementDetail from "../components/LogementDetail";
@@ -11,35 +11,42 @@ const Logements = () => {
   const idUrl = useParams(); // exemple = {id: ':c67ab8a7'}
   const selectedId = idUrl.id.substring(1); // exemple = c67ab8a7
 
-  const accomodations = useFetchAPI("/logements.json");
-
-  const selectedAcc = accomodations.filter((accomodation) =>
+  const accomodations = useFetchAPI("/logements.json").filter((accomodation) =>
     accomodation.id.includes(selectedId)
   );
-  console.log("selected acc = ", selectedAcc);
+  const [isLoading, setIsLoading] = useState(false);
+  const [idExist, setIdExist] = useState(false);
 
-  let count = 0;
-  for (const acc in accomodations) {
-    if (Object.hasOwnProperty.call(accomodations, acc)) {
-      const element = accomodations[acc];
-      if (element.id === selectedId) {
-        count++;
-        // console.log(selectedId, " ", element.id, " ", count);
-      }
+  useEffect(() => {
+    Promise.resolve(accomodations)
+      .then(() => {
+        setIsLoading(true);
+        if (accomodations[0].id === selectedId) {
+          setIdExist(true);
+        }
+      })
+      .catch(() => {
+        return <PageNotFound />;
+      });
+  }, [accomodations, selectedId]);
+
+  if (isLoading) {
+    if (idExist) {
+      return (
+        <div>
+          <Header />
+          {accomodations
+            .filter((accomodation) => accomodation.id.includes(selectedId))
+            .map((accomodation) => (
+              <LogementDetail key={selectedId} accomodation={accomodation} />
+            ))}
+          <Footer />
+        </div>
+      );
+    } else {
+      return <PageNotFound />;
     }
   }
-
-  return (
-    <div>
-      <Header />
-      {accomodations
-        .filter((accomodation) => accomodation.id.includes(selectedId))
-        .map((accomodation) => (
-          <LogementDetail key={selectedId} accomodation={accomodation} />
-        ))}
-      <Footer />
-    </div>
-  );
 };
 
 export default Logements;
